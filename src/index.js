4a7639024fbf513fc6620cf7f91644bf223ebc5a
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-17 01:13:09 +0700

    Completed lession 19

--- src/index.js
+++ src/index.js
@@ -1,5 +1,196 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { createStore, combineReducers } from 'redux';
 import { render } from 'react-dom';
-import { App } from './App';
 
-render(<App />, document.getElementById('root'));
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+        if (state.id !== action.id) {
+          return state
+        }
+
+        return {
+          ...state,
+          completed: !state.completed
+        }
+    default:
+      return state;
+  }
+}
+
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      // Handle blank todo string
+      if (action.text === "") {
+        return state
+      }
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+     default:
+       return state;
+  }
+}
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+})
+
+const store = createStore(todoApp);
+
+const FilterLink = ({
+  filter,
+  currentFilter,
+  children
+}) => {
+  if (filter === currentFilter) {
+    return <span>{children}</span>
+  }
+  return (
+    <a href='#'
+      onClick={e => {
+        e.preventDefault();
+        store.dispatch({
+          type: 'SET_VISIBILITY_FILTER',
+          filter
+        })
+      }}
+    >
+      {children}
+    </a>
+  )
+}
+
+const getVisibleTodos = (
+  todos,
+  filter
+) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos;
+    case 'SHOW_COMPLETED':
+      return todos.filter(
+        t => t.completed
+      )
+    case 'SHOW_ACTIVE':
+      return todos.filter(
+        t => !t.completed
+      )
+  }
+}
+let nextTodoId = 0;
+class TodoApp extends Component {
+  render() {
+    const {
+      todos,
+      visibilityFilter
+    } = this.props
+
+    const visibleTodos = getVisibleTodos(
+      todos,
+      visibilityFilter
+    )
+
+    return (
+      <div>
+        <input ref={node => {
+          this.input = node;
+        }}></input>
+        <button onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              text: this.input.value,
+              id: nextTodoId++
+          });
+          this.input.value = '';
+          this.input.focus();
+        }}>
+          Add Todo
+        </button>
+
+        <ul>
+          {visibleTodos.map(todo =>
+            <li
+              key={todo.id}
+              onClick={() => {
+                store.dispatch({
+                  type: 'TOGGLE_TODO',
+                  id: todo.id
+                })
+              }}
+              style={{
+                textDecoration:
+                todo.completed ?
+                'line-through': 'none'
+              }}
+            >
+            { todo.text}
+            </li>
+          )}
+        </ul>
+        <p>
+          Show:
+          {' '}
+          <FilterLink
+            filter='SHOW_ALL'
+            currentFilter={visibilityFilter}
+          >
+            All
+          </FilterLink>
+          {' '}
+          <FilterLink
+            filter='SHOW_COMPLETED'
+            currentFilter={visibilityFilter}
+          >
+            Completed
+          </FilterLink>
+          {' '}
+          <FilterLink
+            filter='SHOW_ACTIVE'
+            currentFilter={visibilityFilter}
+          >
+            Active
+          </FilterLink>
+          {' '}
+        </p>
+      </div>
+    )
+  }
+}
+
+const renderApp = () => {
+  render(
+    <TodoApp
+      {...store.getState()}
+    />,
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(renderApp);
+
+renderApp();
